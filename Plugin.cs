90ea413fe216e205ec59ecfa2fd6f913085280ed
Macha Celeste <shetah1@yahoo.com>
2025-05-01 12:51:01 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ShowSecondsPlugin;
+
+[BepInPlugin("com.machaceleste.showsecondsplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.showsecondsplugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/ClockPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+[HarmonyPatch]
+class ClockPatch
+{
+    [HarmonyPatch(typeof(Clock), "GetFormatHora")]
+    class GetFormatHoraPatch
+    {
+        static bool Prefix(ref Clock __instance, ref string __result)
+        {
+            __result = __instance.GetDateTime().ToString("HH:mm:ss");
+            return false;
+        }
+    }
+}
